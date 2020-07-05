4348663c61ba42d74b1533713d63c0d12923cce9
Ravi Avula <raviavula@gmail.com>
2020-07-05 14:16:21 -0500

    added tags for http lb

--- main.tf
+++ main.tf
@@ -141,7 +141,7 @@ module "gce-lb-http" {
 
   name              = "external-http-lb"
   project           = var.project_id
-  target_tags       = [google_compute_instance_template.appserver.tags]
+  target_tags       = google_compute_instance_template.appserver.tags
 
   backends = {
     default = {
