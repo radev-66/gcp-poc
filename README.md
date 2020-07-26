9604740e33a6d6d470f04550c4e53e547acbe6c8
Ravi Avula <raviavula@gmail.com>
2020-07-26 15:54:10 -0500

    Added stackdriver changes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gcp-poc with auth and compute engine
+
+
+### Steps or fully automated google project clean-up
+1. packer build 
+2. terraform for creating bucket (for terraform state) or maybe api calls 
+3. finally run terraform 
+
+
+Next Steps: 
+
+1. Application changes for postgres db and logs 
+2. Stackdriver logs on compute engine -- streaming logs instead of files 
+3. Cloud SQL & Cloud SQL proxy on the VM 
+  
--- src/main/java/com/arvi/gcppoc/controller/WelcomeController.java
+++ src/main/java/com/arvi/gcppoc/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.arvi.gcppoc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class.getName());
 	
 	@GetMapping(path = {"/welcome", "/"})
 	public ModelAndView welcome(@AuthenticationPrincipal OidcUser user) {
 		ModelAndView mav = new ModelAndView();
+		logger.info("User : {}", user.getUserInfo().getPreferredUsername());
 		mav.addObject("user", user.getUserInfo());
 		mav.addObject("idToken", user.getIdToken().getTokenValue());
 		mav.setViewName("welcome");
