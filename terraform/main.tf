410cbf3e23d9bc0326bcfd2dcd31a2c17a6d1a88
Ravi Avula <raviavula@gmail.com>
2020-07-26 13:28:44 -0500

    added http lb

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,6 +117,7 @@ resource "google_compute_health_check" "autohealing" {
   }
 }
 
+
 resource "google_compute_instance_group_manager" "appserver-igm" {
   name = "appserver-igm"
 
@@ -142,4 +143,57 @@ resource "google_compute_instance_group_manager" "appserver-igm" {
 }
 
 
+module "gce-lb-http" {
+  source            = "github.com/GoogleCloudPlatform/lb-http/google"
+  version           = "~> 3.1"
 
+  name              = "app-http-lb"
+  project           = var.project_id
+  target_tags       = ["app-server"]
+  backends = {
+    default = {
+      description                     = null
+      protocol                        = "HTTP"
+      port                            = var.service_port
+      port_name                       = var.service_port_name
+      timeout_sec                     = 10
+      connection_draining_timeout_sec = null
+      enable_cdn                      = false
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/"
+        port                = var.service_port
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        {
+          # Each node pool instance group should be added to the backend.
+          group                        = google_compute_instance_group_manager.appserver-igm.instance_group
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        },
+      ]
+    }
+  }
+}
